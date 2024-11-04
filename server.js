c5b6f703462f175bc5f35187f4f2d8ce0d7823cd
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-11-04 16:41:37 -0400

    Set up 404 and index views

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require( 'helmet');
 const corsOptions = require('./config/corsOptions');
+const path = require('path')
 const PORT = process.env.PORT || 3500;
 
 //set up express server
@@ -11,7 +12,25 @@ const app = express();
 
 //middleware
 app.use(helmet());
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
+
+//public accessed files
+app.use('/', express.static(path.join(__dirname, 'public')));
+
+//index
+app.use('/', require('./routes/root'));
+
+//404 for all other non-specified routes
+app.all('*', (req, res)=>{
+    res.status(404)
+    if(req.accepts('html')){
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')){
+        res.json({message: '404 Not Found'})
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+});
 
 
 app.listen(PORT, ()=>{
