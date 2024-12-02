3dda454069c0663a8dcbe4da3c3787e8c58f3d55
leobigott98 <leonardo.bigott@correo.unimet.edu.ve>
2024-12-02 14:21:26 -0400

    added msal graph api webhook authentication

---
+++ auth/serviceAuth/webhookAuth.js
@@ -0,0 +1,34 @@
+const {logEvents} = require('../../middleware/logger');
+const msal = require("@azure/msal-node");
+const { saveMSALToken } = require("../../cache/imapToken");
+
+const clientConfig = {
+    auth: {
+      clientId: process.env.MSAL_WEBHOOK_CLIENT_ID,
+      authority: process.env.MSAL_AUTHORITY,
+      clientSecret: process.env.MSAL_WEBHOOK_CLIENT_SECRET,
+    },
+  };
+  
+  const cca = new msal.ConfidentialClientApplication(clientConfig);
+  
+  const clientCredentialRequest = {
+    scopes: [process.env.MSAL_WEBHOOK_SCOPE], // replace with your resource
+  };
+  
+  const msalAuthenticate = async () => {
+    try{
+      console.log('attempting token request')
+      const response = await cca.acquireTokenByClientCredential(clientCredentialRequest)
+      const authToken = response.accessToken;
+      logEvents(`MSAL Response: ${JSON.stringify(response)}`, "msalLog.log");
+      await saveMSALToken(authToken);
+      return authToken;
+    } catch(error) {
+        logEvents(`MSAL Error: ${JSON.stringify(error)}`, "msalErrorLog.log");
+        console.log(JSON.stringify(error));
+        return null;
+      };
+  };
+  
+  module.exports = { msalAuthenticate };
---
+++ cache/msalToken.js
@@ -0,0 +1,28 @@
+const {client} = require('../config/redisConn');
+
+const saveMSALToken = async(token)=>{
+    try{
+        await client.set('msal_auth_token', token, { EX: 3600});
+        console.log('MSAL Token stored in Redis with expiration');
+    }catch(err){
+        console.log(`Error storing MSAL token in Redis: ${err}`);
+        logEvents(`Error storing MSAL token in Redis: ${err}`, 'errLog.log');
+    }
+};
+
+const getToken = async()=>{
+    try{
+        const token = await client.get('msal_auth_token');
+        if(token){
+            return token;
+        }else{
+            //Token expired or not found; handle re-authentication
+            console.log('MSAL Token expired, re-authenticating...');
+        }
+    }catch(err){
+        console.log('Error retrieving from Redis:', err);
+        logEvents(`Error retrieving from Redis: ${err}`, errLog.log);
+    }
+}
+
+module.exports = {saveMSALToken, getToken}
